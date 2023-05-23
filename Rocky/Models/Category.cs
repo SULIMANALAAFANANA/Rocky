53e447909e92fe8767393bbe9427b2a3b163b0e1
HP <HP@DESKTOP-VP5V6ES>
2023-05-23 18:15:57 +0200

    Add project files.

---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [System.ComponentModel.DataAnnotations.Required]
+        [Range(1,int.MaxValue,ErrorMessage ="Display Order for category must be greater than 0")]
+        public int DisplayOrder { get; set; }
+    }
+}
